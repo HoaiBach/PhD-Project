8a83fed29dc04c71506c4b82296f065ee53a9536
Hoai Bach <bachbk1611@gmail.com>
2017-10-04 17:18:54 +1300

    Change the main test

--- MultiObjectiveFeatureSelection/src/Mains/MainTest.java
+++ MultiObjectiveFeatureSelection/src/Mains/MainTest.java
@@ -8,8 +8,8 @@ import java.util.Random;
 
 public class MainTest {
 
-	public static void main(String[] args) throws FileNotFoundException{	
-		for(int i=1; i<=1;i++){
+	public static void main(String[] args) throws FileNotFoundException{
+		for(int i=1; i<=10;i++){
 			String[] arg = new String[]{"7",i+"","0"};
 			Main.main(arg);
 		}
